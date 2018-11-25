e46243d73e54bb51a25289742414ecf7606dbd47
firas kordoghli <firas9@rocketmail.com>
2018-11-25 14:16:26 +0100

    Initial commit

---
+++ app/src/main/java/kordoghli/firas/troc/MainActivity.kt
@@ -0,0 +1,12 @@
+package kordoghli.firas.troc
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    //hello git
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
