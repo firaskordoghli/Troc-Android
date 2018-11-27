19e09d4842fc82516d69907d48e648cd2ce46e9e
firas kordoghli <firas9@rocketmail.com>
2018-11-27 02:27:18 +0100

    UI menu

---
+++ app/src/main/java/kordoghli/firas/troc/HomeActivity.kt
@@ -0,0 +1,50 @@
+package kordoghli.firas.troc
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v7.app.ActionBar
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class HomeActivity : AppCompatActivity() {
+
+    lateinit var toolbar: ActionBar
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_songs -> {
+                toolbar.title = "Home"
+                val homeFragment = HomeFragment.newInstance()
+                openFragment(homeFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_albums -> {
+                toolbar.title = "Profile"
+                val profileFragment = ProfileFragment.newInstance()
+                openFragment(profileFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        toolbar = supportActionBar!!
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+
+}
--- app/src/main/java/kordoghli/firas/troc/MainActivity.kt
+++ app/src/main/java/kordoghli/firas/troc/MainActivity.kt
@@ -2,11 +2,13 @@ package kordoghli.firas.troc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
-    //hello git
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
